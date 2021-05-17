4c4a07221101cacd54ea2d6bfe86e59dd817aee2
Tombstoner <81688030+Tombstoner@users.noreply.github.com>
2021-05-17 22:01:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({extended : true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/", function(req, res) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = (num1 + num2);
+    res.send("The sum of the two number is : "+ result);
+});
+
+app.listen(3000, function() {
+    console.log("Server has started on port 3000.");
+});
